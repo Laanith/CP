cdf2422727027f37ba45bad31e7013e124d3c996
Laanith <lanithchouhan2004@gmail.com>
2024-01-21 23:23:25 +0530

    Initial Commit

---
+++ CodeForces/443A_AntonAndLetters_CF.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+ A. Anton and Letters
+
+Recently, Anton has found a set. The set consists of small English letters. Anton carefully wrote out all the letters from the set in one line, separated by a comma. He also added an opening curved bracket at the beginning of the line and a closing curved bracket at the end of the line.
+
+Unfortunately, from time to time Anton would forget writing some letter and write it again. He asks you to count the total number of distinct letters in his set.
+
+Input
+The first and the single line contains the set of letters. The length of the line doesn't exceed 1000. It is guaranteed that the line starts from an opening curved bracket and ends with a closing curved bracket. Between them, small English letters are listed, separated by a comma. Each comma is followed by a space.
+
+Output
+Print a single number — the number of distinct letters in Anton's set.
+*/
+
+class Solution{
+public :
+
+};
+ 
+ 
+int main(){
+string x;
+set<char> s;
+getline(cin, x);
+for(auto c : x){
+    if(c-'a'>=0 && 'z'-c>=0 && c != ' '){
+        s.insert(c);
+    }
+}
+cout << s.size() << endl;
+
+return 0 ;
+}
