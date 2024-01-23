9376d4299616ba2dba52caa808a6b1bee55bb8e0
Laanith <lanithchouhan2004@gmail.com>
2024-01-23 21:51:10 +0530

    Initial Commit

---
+++ CodeForces/158A_NextRound_CF.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define pb push_back()
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+ "Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+
+Input
+The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
+
+The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+
+Output
+Output the number of participants who advance to the next round.
+*/
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int n, k;
+cin >> n >> k;
+vi nums;
+for (int i = 0; i < n; i++){
+    int r;
+    cin >> r;
+    nums.push_back(r);
+}
+
+// sort(nums.begin(), nums.end());
+
+int i = 0;
+int score = nums[k - 1];
+
+for(auto x : nums){
+    if(x >= score && x > 0){
+                i++;
+    }
+}
+
+cout << i << endl;
+
+return 0;
+}
