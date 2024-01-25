d4e63af936781691ac52fb5b24aceafe56f36d5a
Laanith <lanithchouhan2004@gmail.com>
2024-01-25 21:36:00 +0530

    Initial Commit

---
+++ CodeForces/50A_DominoPiling_CF.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
+
+1. Each domino completely covers two squares.
+
+2. No two dominoes overlap.
+
+3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+
+Find the maximum number of dominoes, which can be placed under these restrictions.
+
+Input
+In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
+
+Output
+Output one number — the maximal number of dominoes, which can be placed.
+*/
+
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int m, n;
+cin >> m >> n;
+cout << m * n / 2;
+
+return 0 ;
+}
