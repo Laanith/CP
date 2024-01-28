d6192ddd858ed2c74ca81e7431834f04369b8294
Laanith <lanithchouhan2004@gmail.com>
2024-01-28 21:03:23 +0530

    Initial Commit

---
+++ CodeForces/71A_WayTooLongWords_CF.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+A. Way Too Long Words
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
+
+Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
+
+This abbreviation is made like this: we write down the first and the last letter of a word and between them we write the number of letters between the first and the last letters. That number is in decimal system and doesn't contain any leading zeroes.
+
+Thus, "localization" will be spelt as "l10n", and "internationalization» will be spelt as "i18n".
+
+You are suggested to automatize the process of changing the words with abbreviations. At that all too long words should be replaced by the abbreviation and the words that are not too long should not undergo any changes.
+
+Input
+The first line contains an integer n (1 ≤ n ≤ 100). Each of the following n lines contains one word. All the words consist of lowercase Latin letters and possess the lengths of from 1 to 100 characters.
+
+Output
+Print n lines. The i-th line should contain the result of replacing of the i-th word from the input data.
+*/
+
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int t;
+cin >> t;
+while(t--){
+    string x;
+    cin >> x;
+    if(x.length() <= 10)
+        cout << x << endl;
+    else {
+        cout << x[0] + to_string(x.length() - 2) + x[x.length() - 1] << endl;
+    }
+}
+
+return 0 ;
+}
