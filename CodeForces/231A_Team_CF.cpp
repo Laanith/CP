7f75ac978553f66fa3af70fe253a38e5b903dc4c
Laanith <lanithchouhan2004@gmail.com>
2024-01-29 18:42:17 +0530

    Initial Commit

---
+++ CodeForces/231A_Team_CF.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+A. Team
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
+
+This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find the number of problems for which they will write a solution.
+
+Input
+The first input line contains a single integer n (1 ≤ n ≤ 1000) — the number of problems in the contest. Then n lines contain three integers each, each integer is either 0 or 1. If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't sure. The second number shows Vasya's view on the solution, the third number shows Tonya's view. The numbers on the lines are separated by spaces.
+
+Output
+Print a single integer — the number of problems the friends will implement on the contest.
+*/
+
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+int t;
+int count = 0;
+cin >> t;
+while(t--){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a+b+c>=2)
+        count++;
+}
+cout << count << endl;
+
+return 0 ;
+}
