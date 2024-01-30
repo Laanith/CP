789b6d168cc70db487f15197c4afe069747280f6
Laanith <lanithchouhan2004@gmail.com>
2024-01-30 19:52:08 +0530

    Initial Commit

---
+++ CodeForces/236A_BoyOrGirl_CF.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+A. Boy or Girl
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Those days, many boys use beautiful girls' photos as avatars in forums. So it is pretty hard to tell the gender of a user at the first glance. Last year, our hero went to a forum and had a nice chat with a beauty (he thought so). After that they talked very often and eventually they became a couple in the network.
+
+But yesterday, he came to see "her" in the real world and found out "she" is actually a very strong man! Our hero is very sad and he is too tired to love again now. So he came up with a way to recognize users' genders by their user names.
+
+This is his method: if the number of distinct characters in one's user name is odd, then he is a male, otherwise she is a female. You are given the string that denotes the user name, please help our hero to determine the gender of this user by his method.
+
+Input
+The first line contains a non-empty string, that contains only lowercase English letters — the user name. This string contains at most 100 letters.
+
+Output
+If it is a female by our hero's method, print "CHAT WITH HER!" (without the quotes), otherwise, print "IGNORE HIM!" (without the quotes).
+*/
+
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+string x;
+cin >> x;
+set<char> s;
+for(auto c: x)
+    s.insert(c);
+
+if(s.size() %2 )
+    cout << "IGNORE HIM!" << endl;
+else
+    cout << "CHAT WITH HER!" << endl;
+
+return 0 ;
+}
