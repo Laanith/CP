1e79b0c85d684c0cfc3594aa8fc11c1e898d0005
Laanith <lanithchouhan2004@gmail.com>
2024-01-31 21:17:00 +0530

    Initial Commit

---
+++ CodeForces/281A_WordCapitalization_CF.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+
+/*!SECTION : Problem Description ::
+A. Word Capitalization
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+*/
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+string x;
+cin >> x;
+if(x[0]>='a' && x[0]<='z'){
+    x[0] = char(x[0] - abs('A' - 'a'));
+}
+cout << x << endl;
+
+return 0;
+}
