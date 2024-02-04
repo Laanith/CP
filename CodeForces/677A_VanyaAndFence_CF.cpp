b5eb237c88644de7d82a6e51d55fdd72925ec1fd
Laanith <lanithchouhan2004@gmail.com>
2024-02-04 13:29:11 +0530

    Initial Commit

---
+++ CodeForces/677A_VanyaAndFence_CF.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define ll long long
+#include<algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int,int>>
+#define PYES cout<<"YES"<<endl
+#define PNO cout<<"NO"<<endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int,int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A){cout << endl;for(auto x : A) cout << x <<' ';cout << endl;}
+void pint(int A){cout<<endl<<A<<endl;}
+void pp(pair<int,int> p){cout<<p.first<<' '<<p.second<<endl;}
+ 
+ 
+/*!SECTION : Problem Description ::
+A. Vanya and Fence
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Vanya and his friends are walking along the fence of height h and they do not want the guard to notice them. In order to achieve this the height of each of the friends should not exceed h. If the height of some person is greater than h he can bend down and then he surely won't be noticed by the guard. The height of the i-th person is equal to ai.
+
+Consider the width of the person walking as usual to be equal to 1, while the width of the bent person is equal to 2. Friends want to talk to each other while walking, so they would like to walk in a single row. What is the minimum width of the road, such that friends can walk in a row and remain unattended by the guard?
+
+Input
+The first line of the input contains two integers n and h (1 ≤ n ≤ 1000, 1 ≤ h ≤ 1000) — the number of friends and the height of the fence, respectively.
+
+The second line contains n integers ai (1 ≤ ai ≤ 2h), the i-th of them is equal to the height of the i-th person.
+
+Output
+Print a single integer — the minimum possible valid width of the road.
+*/
+ 
+ 
+
+ 
+ 
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+int n,h;
+cin>>n>>h;
+int sum=0;
+while(n--){
+    int c;
+    cin>>c;
+    if(c>h){
+        sum+=2;
+    }
+    else sum++;
+}
+cout<<sum<<endl;
+ 
+return 0 ;
+}
