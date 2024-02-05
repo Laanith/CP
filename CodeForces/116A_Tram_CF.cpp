ddadc8f60a2c938633f2502055c0a433bbf587cb
Laanith <lanithchouhan2004@gmail.com>
2024-02-05 23:29:57 +0530

    Initial Commit

---
+++ CodeForces/116A_Tram_CF.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+
+/*!SECTION : Problem Description ::
+Linear Kingdom has exactly one tram line. It has n stops, numbered from 1 to n in the order of tram's movement. At the i-th stop ai passengers exit the tram, while bi passengers enter it. The tram is empty before it arrives at the first stop. Also, when the tram arrives at the last stop, all passengers exit so that it becomes empty.
+
+Your task is to calculate the tram's minimum capacity such that the number of people inside the tram at any time never exceeds this capacity. Note that at each stop all exiting passengers exit before any entering passenger enters the tram.
+
+Input
+The first line contains a single number n (2 ≤ n ≤ 1000) — the number of the tram's stops.
+
+Then n lines follow, each contains two integers ai and bi (0 ≤ ai, bi ≤ 1000) — the number of passengers that exits the tram at the i-th stop, and the number of passengers that enter the tram at the i-th stop. The stops are given from the first to the last stop in the order of tram's movement.
+
+The number of people who exit at a given stop does not exceed the total number of people in the tram immediately before it arrives at the stop. More formally, . This particularly means that a1 = 0.
+At the last stop, all the passengers exit the tram and it becomes empty. More formally, .
+No passenger will enter the train at the last stop. That is, bn = 0.
+Output
+Print a single integer denoting the minimum possible capacity of the tram (0 is allowed)
+*/
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    int sum=0;
+    int max_sum = imi;
+    cin>>t;
+    while (t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        sum = sum-a+b;
+        max_sum = max(max_sum,sum);
+    }
+    cout<<max_sum<<endl;
+    return 0;
+}
