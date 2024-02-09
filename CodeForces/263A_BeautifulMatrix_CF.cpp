e654a2d2514f70a4c5f86b81d18fc270b51f6d62
Laanith <lanithchouhan2004@gmail.com>
2024-02-09 10:09:33 +0530

    Initial Commit

---
+++ CodeForces/263A_BeautifulMatrix_CF.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A){for(auto v : A){pv(v);}}
+
+/*!SECTION : Problem Description ::
+You've got a 5 × 5 matrix, consisting of 24 zeroes and a single number one. Let's index the matrix rows by numbers from 1 to 5 from top to bottom, let's index the matrix columns by numbers from 1 to 5 from left to right. In one move, you are allowed to apply one of the two following transformations to the matrix:
+
+Swap two neighboring matrix rows, that is, rows with indexes i and i + 1 for some integer i (1 ≤ i < 5).
+Swap two neighboring matrix columns, that is, columns with indexes j and j + 1 for some integer j (1 ≤ j < 5).
+You think that a matrix looks beautiful, if the single number one of the matrix is located in its middle (in the cell that is on the intersection of the third row and the third column). Count the minimum number of moves needed to make the matrix beautiful.
+
+Input
+The input consists of five lines, each line contains five integers: the j-th integer in the i-th line of the input represents the element of the matrix that is located on the intersection of the i-th row and the j-th column. It is guaranteed that the matrix consists of 24 zeroes and a single number one.
+
+Output
+Print a single integer — the minimum number of moves needed to make the matrix beautiful.
+*/
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vvi A(5, vi(5,0));
+
+    int dis = imi;
+
+    for(int i = 0 ; i < A.siz(); i++){
+        for (int j=0; j < A[i].siz(); j++){
+           cin>>A[i][j];
+
+           if(A[i][j]==1){
+                dis = abs(i-2)+abs(j-2);
+           }
+        }
+    }
+
+    pint(dis);
+    
+
+    return 0;
+}
