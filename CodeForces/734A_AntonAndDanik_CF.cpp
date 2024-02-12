898d8783bc85089472410eabe37b44dcd96e2df9
Laanith <lanithchouhan2004@gmail.com>
2024-02-12 20:34:18 +0530

    Initial Commit

---
+++ CodeForces/734A_AntonAndDanik_CF.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+A. Anton and Danik
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Anton likes to play chess, and so does his friend Danik.
+
+Once they have played n games in a row. For each game it's known who was the winner — Anton or Danik. None of the games ended with a tie.
+
+Now Anton wonders, who won more games, he or Danik? Help him determine this.
+
+Input
+The first line of the input contains a single integer n (1 ≤ n ≤ 100 000) — the number of games played.
+
+The second line contains a string s, consisting of n uppercase English letters 'A' and 'D' — the outcome of each of the games. The i-th character of the string is equal to 'A' if the Anton won the i-th game and 'D' if Danik won the i-th game.
+
+Output
+If Anton won more games than Danik, print "Anton" (without quotes) in the only line of the output.
+
+If Danik won more games than Anton, print "Danik" (without quotes) in the only line of the output.
+
+If Anton and Danik won the same number of games, print "Friendship" (without quotes).
+*/
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll a=0,d=0;
+    string x;
+    ll r;
+    cin>>r>>x;
+    for(auto c :x){
+        if(c=='A') a++;
+        else d++;
+    }
+    if(a>d) cout<<"Anton"<<endl;
+    else if(a==d) cout<<"Friendship"<<endl;
+    else cout<<"Danik"<<endl;
+    return 0;
+}
