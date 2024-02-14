b7dfbf91b25cabe2c4034bc0cf1d5b83bb349ae3
Laanith <lanithchouhan2004@gmail.com>
2024-02-14 12:29:47 +0530

    Initial Commit

---
+++ CodeForces/1030A_InSearchOfAnEasyProblem_CF.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+When preparing a tournament, Codeforces coordinators try treir best to make the first problem as easy as possible. This time the coordinator had chosen some problem and asked n
+ people about their opinions. Each person answered whether this problem is easy or hard.
+
+If at least one of these n
+ people has answered that the problem is hard, the coordinator decides to change the problem. For the given responses, check if the problem is easy enough.
+
+Input
+The first line contains a single integer n
+ (1≤n≤100
+) — the number of people who were asked to give their opinions.
+
+The second line contains n
+ integers, each integer is either 0
+ or 1
+. If i
+-th integer is 0
+, then i
+-th person thinks that the problem is easy; if it is 1
+, then i
+-th person thinks that the problem is hard.
+
+Output
+Print one word: "EASY" if the problem is easy according to all responses, or "HARD" if there is at least one person who thinks the problem is hard.
+
+You may print every letter in any register: "EASY", "easy", "EaSY" and "eAsY" all will be processed correctly.
+
+
+*/
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string x="EASY";
+    int n;
+    cin>>n;
+    while(n--){
+        int r;
+        cin>>r;
+        if(r==1) {
+            x="HARD";
+            break;
+        }
+    }
+
+    cout<<x<<endl;
+
+    return 0;
+}
