b11ecf2caa2dd98d96fd56723303cc6d65e0e768
Laanith <lanithchouhan2004@gmail.com>
2024-02-17 23:08:18 +0530

    Initial Commit

--- CodeForces/723A_TheNewYearMeetingFriends_CF.cpp
+++ CodeForces/723A_TheNewYearMeetingFriends_CF.cpp
@@ -1,12 +1,64 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
 using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
 
-int main() {
-    int x1,x2,x3,mean,sum=0;
+/*!SECTION : Problem Description ::
+A. The New Year: Meeting Friends
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+There are three friend living on the straight line Ox in Lineland. The first friend lives at the point x1, the second friend lives at the point x2, and the third friend lives at the point x3. They plan to celebrate the New Year together, so they need to meet at one point. What is the minimum total distance they have to travel in order to meet at some point and celebrate the New Year?
+
+It's guaranteed that the optimal answer is always integer.
+
+Input
+The first line of the input contains three distinct integers x1, x2 and x3 (1 ≤ x1, x2, x3 ≤ 100) — the coordinates of the houses of the first, the second and the third friends respectively.
+
+Output
+Print one integer — the minimum total distance the friends need to travel in order to meet together.
+*/
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int x1,x2,x3;
     cin>>x1>>x2>>x3;
- +abs(x2-mean) +abs(x3-mean));
- 
     cout<<-min(x1,min(x2,x3))+max(x1,max(x2,x3));
-    
+
+    return 0;
 }
