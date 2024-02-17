dd6cd3cfd9ead0ac38484413ab85a17588c900b6
Laanith Chouhan <99798440+Laanith@users.noreply.github.com>
2024-02-17 22:23:28 +0530

    Latest Commit

---
+++ CodeForces/723A_TheNewYearMeetingFriends_CF.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x1,x2,x3,mean,sum=0;
+    cin>>x1>>x2>>x3;
+ +abs(x2-mean) +abs(x3-mean));
+ 
+    cout<<-min(x1,min(x2,x3))+max(x1,max(x2,x3));
+    
+}
