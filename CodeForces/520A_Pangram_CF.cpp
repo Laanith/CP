2d52a6ea10fb3a86650ef668fe836bc859cd4368
Laanith <lanithchouhan2004@gmail.com>
2024-02-25 17:27:46 +0530

    Initial Commit

---
+++ CodeForces/520A_Pangram_CF.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+A word or a sentence in some language is called a pangram if all the characters of the alphabet of this language appear in it at least once. Pangrams are often used to demonstrate fonts in printing or test the output devices.
+
+You are given a string consisting of lowercase and uppercase Latin letters. Check whether this string is a pangram. We say that the string contains a letter of the Latin alphabet if this letter occurs in the string in uppercase or lowercase.
+
+Input
+The first line contains a single integer n (1 ≤ n ≤ 100) — the number of characters in the string.
+
+The second line contains the string. The string consists only of uppercase and lowercase Latin letters.
+
+Output
+Output "YES", if the string is a pangram and "NO" otherwise.
+*/
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    string x;
+    set<char> s;
+    cin>>n>>x;
+    if(n<26) PNO;
+    else {
+        for(auto c:x){
+            s.insert(tolower(c));
+        }
+        if(s.size()==26) PYES;
+        else PNO;
+    }
+    return 0;
+}
