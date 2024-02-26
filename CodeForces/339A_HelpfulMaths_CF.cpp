5a262e7e2ebb41e309891f87847ab35916f9d258
Laanith <lanithchouhan2004@gmail.com>
2024-02-26 21:40:12 +0530

    Initial Commit

---
+++ CodeForces/339A_HelpfulMaths_CF.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+Xenia the beginner mathematician is a third year student at elementary school. She is now learning the addition operation.
+
+The teacher has written down the sum of multiple numbers. Pupils should calculate the sum. To make the calculation easier, the sum only contains numbers 1, 2 and 3. Still, that isn't enough for Xenia. She is only beginning to count, so she can calculate a sum only if the summands follow in non-decreasing order. For example, she can't calculate sum 1+3+2+1 but she can calculate sums 1+1+2 and 3+3.
+
+You've got the sum that was written on the board. Rearrange the summans and print the sum in such a way that Xenia can calculate the sum.
+
+Input
+The first line contains a non-empty string s — the sum Xenia needs to count. String s contains no spaces. It only contains digits and characters "+". Besides, string s is a correct sum of numbers 1, 2 and 3. String s is at most 100 characters long.
+
+Output
+Print the new sum that Xenia can count.
+*/
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string x;
+    cin>>x;
+    vector<char> v;
+    for(int i=0; i < x.length(); i++){
+        if(x[i]=='+')
+        {
+            continue;
+        }
+        else {
+            v.pb(x[i]);
+        }
+    }
+    sort(v.begin(),v.end());
+    for(int i =0; i < v.size()-1;i++){
+        cout<<v[i]<<'+';
+    }
+    cout<<v[v.size()-1]<<endl;
+
+ 
+    
+    return 0;
+}
