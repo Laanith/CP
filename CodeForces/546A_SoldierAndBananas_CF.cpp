292b40ad83f3d3f015b43ffeecce3a271cdf38dc
Laanith <lanithchouhan2004@gmail.com>
2024-02-27 18:42:45 +0530

    Initial Commit

---
+++ CodeForces/546A_SoldierAndBananas_CF.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define ll long long
+#include <algorithm>
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define PYES cout << "YES" << endl
+#define PNO cout << "NO" << endl
+#define vs vector<string>
+#define qi queue<int>
+#define si stack<int>
+#define pi pair<int, int>
+#define ima INT_MAX
+#define imi INT_MIN
+#define siz size()
+#define vvi vector<vector<int>>
+#define pb push_back
+using namespace std;
+void pv(vi A)
+{
+    cout << endl;
+    for (auto x : A)
+        cout << x << ' ';
+    cout << endl;
+}
+void pint(int A) { cout << endl
+                        << A << endl; }
+void pp(pair<int, int> p) { cout << p.first << ' ' << p.second << endl; }
+void pvvi(vvi A)
+{
+    for (auto v : A)
+    {
+        pv(v);
+    }
+}
+
+/*!SECTION : Problem Description ::
+A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
+
+He has n dollars. How many dollars does he have to borrow from his friend soldier to buy w bananas?
+
+Input
+The first line contains three positive integers k, n, w (1  ≤  k, w  ≤  1000, 0 ≤ n ≤ 109), the cost of the first banana, initial number of dollars the soldier has and number of bananas he wants.
+
+Output
+Output one integer — the amount of dollars that the soldier must borrow from his friend. If he doesn't have to borrow money, output 0.
+*/
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int k,n,w;
+    cin>>k>>n>>w;
+    int cost = (w*(w+1)*k)/2;
+    if(cost-n<0) pint(0);
+    else pint(cost-n);
+    
+
+    return 0;
+}
